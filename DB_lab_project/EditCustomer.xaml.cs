0c43378e27b73d6059bccc33e54d664a0c6e309a
saharnafisi <sahar.nafisi95@gmail.com>
2017-07-09 09:06:41 +0430

    edit customer added

--- DB_lab_project/EditCustomer.xaml.cs
+++ DB_lab_project/EditCustomer.xaml.cs
@@ -28,5 +28,20 @@ namespace DB_lab_project
             conn = sqlconn;
             InitializeComponent();
         }
+
+        private void btn_enterEditedCustomerID_Click(object sender, RoutedEventArgs e)
+        {
+            bool valid = Functions.Validate(txb_editCustID);
+            if (valid)
+            {
+                label1.Visibility = System.Windows.Visibility.Visible;
+                label2.Visibility = System.Windows.Visibility.Visible;
+                label3.Visibility = System.Windows.Visibility.Visible;
+                txb_newCustAddress.Visibility = System.Windows.Visibility.Visible;
+                txb_newCustName.Visibility = System.Windows.Visibility.Visible;
+                txb_newCustPhone.Visibility = System.Windows.Visibility.Visible;
+                btn_editCustomer.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
     }
 }
